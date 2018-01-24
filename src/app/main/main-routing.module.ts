8d67c21f1cac04aa4e7401afe7eb8620b63c8d8d
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-01-24 11:29:29 +0100

    Updating app

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuardService } from 'ontimize-web-ng2';
+import { AuthGuardService } from 'ontimize-web-ngx';
 
 import { MainComponent } from './main.component';
 import { HomeModule } from './home/home.module';
@@ -10,19 +10,18 @@ export function loadHomeModule() {
 }
 
 
-export const routes: Routes = [
-  {
-    path: 'main',
-    component: MainComponent,
-    canActivate: [AuthGuardService],
-    children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      {
-        path: 'home',
-        loadChildren: loadHomeModule
-      }
-    ]
-  }
+export const routes: Routes = [{
+  path: '',
+  component: MainComponent,
+  canActivate: [AuthGuardService],
+  children: [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {
+      path: 'home',
+      loadChildren: loadHomeModule
+    }
+  ]
+}
 ];
 
 @NgModule({
--- src/app/shared/app.menu.config.ts
+++ src/app/shared/app.menu.config.ts
@@ -1,7 +1,4 @@
-import {
-  MenuRootItem,
-
-} from 'ontimize-web-ng2';
+import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' }
